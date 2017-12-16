e3582a0f3f620b42ee16cd717b4d98f1e79539a7
dxwc <dxwc@users.noreply.github.com>
2017-12-16 18:24:19 -0500

    func to create seats free entry as needed

---
+++ database/postgres_functions/ensure_seats_free_set.sql
@@ -0,0 +1,31 @@
+-- if no entry found : create, else do nothing
+
+DROP FUNCTION IF EXISTS ensure_seats_free_set(INTEGER, INTEGER, DATE);
+CREATE FUNCTION ensure_seats_free_set
+(
+    _train_id INTEGER,
+    _segment_id INTEGER,
+    _of_date DATE DEFAULT NOW()
+)
+RETURNS VOID AS $$
+BEGIN
+    BEGIN
+        INSERT INTO seats_free
+        (
+            train_id,
+            segment_id,
+            of_date,
+            num_of_free_seats
+        )
+        VALUES
+        (
+            _train_id,
+            _segment_id,
+            _of_date,
+            448
+        );
+    EXCEPTION WHEN OTHERS THEN
+        RETURN;
+    END;
+END;
+$$ LANGUAGE PLPGSQL;
