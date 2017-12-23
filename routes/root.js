b0e0584aadd804ac1addba55bb6963ae3756a80a
Mikhail Kreytser <Mikhail.kreyster@gmail.com>
2017-12-23 16:23:15 -0500

    reservation lookup error message

--- routes/root.js
+++ routes/root.js
@@ -22,12 +22,19 @@ router.get('/reservation/lookUp', (req, res) =>
 router.post('/reservation/lookUp', (req, res) =>{
     db.query(`SELECT * FROM passenger WHERE email='${req.body.email.toLowerCase()}' AND name = '${req.body.first_name.toLowerCase()+' '+req.body.last_name.toLowerCase()}'`)
     .then((passenger)=>{
-        db.query(`SELECT * FROM reservation WHERE id=${req.body.reservation_id} AND passenger_id=${passenger[0].id}`)
-        .then((reservation)=>{
-            res.render('reservation/show',{reservation : reservation[0]});
-        }).catch((err) =>{
-        res.json(err);
-        });
+        if(passenger[0] == undefined)
+            res.render('reservation/show',{reservation : false, error: "Passenger Not Found. Wrong Name/Email."});
+        else{
+            db.query(`SELECT * FROM reservation WHERE id=${req.body.reservation_id} AND passenger_id=${passenger[0].id}`)
+            .then((reservation)=>{
+                if(reservation[0] == undefined)
+                    res.render('reservation/show',{reservation : false, error: "Reservation Not Found. Wrong Reservation ID."});
+                else
+                    res.render('reservation/show',{reservation : reservation[0]});
+            }).catch((err) =>{
+            res.json(err);
+            });
+        }
     }).catch((err) =>{
         res.json(err);
     });
@@ -56,6 +63,7 @@ router.get('/book/:train_id/:arrival_time/:date/:from_station/:to_station', (req
                 var senior_first_class  = (base_fare/2*4.25).toFixed(2);
                 var adult_first_class   = (base_fare/2*4.75).toFixed(2);
                 var child_first_class   = (base_fare/2*4).toFixed(2);
+
                 res.render('book/new', {train: req.params.train_id, departureDate : req.params.date, departureTime:req.params.arrival_time,
                                         departureStation: from_station[0].name, arrivalStation:to_station[0].name, military_reqular,senior_reqular,
                                         adult_reqular, pet_reqular, child_reqular, military_first_class, senior_first_class, adult_first_class, child_first_class});
