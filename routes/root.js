0ad71c564c8707539041be0be26b47c47f4e666a
Mikhail Kreytser <Mikhail.kreyster@gmail.com>
2017-12-23 01:18:30 -0500

    functional My reservation search

--- routes/root.js
+++ routes/root.js
@@ -19,10 +19,18 @@ router.get('/reservations', (req, res) =>
     res.render('reservation/lookUp');
 });
 
-router.post('/reservations', (req, res) =>
-{
-    //LOOK UP THE RESERVSTION IN DB
-    res.render('reservation/show');
+router.post('/reservations', (req, res) =>{
+    db.query(`SELECT * FROM passenger WHERE email='${req.body.email.toLowerCase()}' AND name = '${req.body.first_name.toLowerCase()+' '+req.body.last_name.toLowerCase()}'`)
+    .then((passenger)=>{
+        db.query(`SELECT * FROM reservation WHERE id=${req.body.reservation_id} AND passenger_id=${passenger[0].id}`)
+        .then((reservation)=>{
+            res.render('reservation/show',{reservation : reservation[0]});
+        }).catch((err) =>{
+        res.json(err);
+        });
+    }).catch((err) =>{
+        res.json(err);
+    });
 });
 
 router.get('/book/:train_id/:arrival_time/:date/:from_station/:to_station', (req, res) =>
