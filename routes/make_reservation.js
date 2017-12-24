51ce79f72b66634e661c8dc4a4769270f37b7fc6
dxwc <dxwc@users.noreply.github.com>
2017-12-24 14:05:30 -0500

    fixed loss of session vars on some browser's redirect

--- routes/make_reservation.js
+++ routes/make_reservation.js
@@ -1,23 +1,11 @@
 let router = require('express').Router();
 let db = require('../database/connect');
 
-router.get('/make_reservation', (req, res) =>
+router.post('/make_reservation', (req, res) =>
 {
-    if
-    (
-        req.session === undefined ||
-        req.session.date === undefined ||
-        req.session.schedule === undefined ||
-        req.session.from_station === undefined ||
-        req.session.to_station === undefined ||
-        req.session.base_fare === undefined ||
-        req.session.distance === undefined ||
-        req.session.train_id === undefined ||
-        req.session.is_first_class_seats_free === undefined
-    )
-    {
-        return res.redirect(301, '/');
-    }
+    req.session.train_id = req.body.train_id;
+    req.session.arrival_time = req.body.arrival_time;
+    req.session.is_first_class_seats_free = req.body.is_first_class_seats_free;
 
     db.query
     (
@@ -38,14 +26,6 @@ router.get('/make_reservation', (req, res) =>
         console.log('Error:\n', err);
         res.render('reservation', req.session);
     });
-});
-
-router.post('/make_reservation', (req, res) =>
-{
-    req.session.train_id = req.body.train_id;
-    req.session.arrival_time = req.body.arrival_time;
-    req.session.is_first_class_seats_free = req.body.is_first_class_seats_free;
-    res.redirect(301, '/make_reservation');
 });
 
 module.exports = router;
